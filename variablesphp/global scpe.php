cb8fd76eff7fa1eaef462ebea946ffb9fb51d07a
saifahs <50007871+saifahs@users.noreply.github.com>
2019-07-01 03:48:53 -0700

    homework

---
+++ variablesphp/global scpe.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+    $x = 5;
+
+    function myTest() {
+
+    	echo "<p>Variable x inside function is: $x</p>";
+    }
+
+    myTest();
+
+        echo "<p>Variable x outside function is : $x</p>";
+?>
+	
+</body>
+</html>
---
+++ variablesphp/output variables 2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+    $txt = "W3Schools.com";
+    echo "I love " . $txt . "!";
+?>
+
+</body>
+</html>
