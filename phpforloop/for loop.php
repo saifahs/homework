6dec7eb8400dacc8db13006a4788c7fc2cf4ab33
saifahs <50007871+saifahs@users.noreply.github.com>
2019-07-10 10:32:11 +0500

    homework

---
+++ phpforloop/for loop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+
+<?php
+
+    $colors = array("red", "white", "blue", "black", "gray"); 
+
+    for ($x = 0; $x < count($colors); $x++) { 
+     	
+     	echo "$colors[$x] <br>";
+    }
+
+?>
+
+</body>
+</html>
